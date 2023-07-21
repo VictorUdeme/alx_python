38d83d9d4ad900fe773b00fb55dc1b8d664c4b04
VictorUdeme <snazzydo9@gmail.com>
2023-07-20 18:00:55 -0700

    Task 006

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,5 @@
 for tens_digit in range(0, 10):
     for units_digit in range(tens_digit + 1, 10):
-        print(f"{tens_digit:01d}{units_digit:01d}", end=", ")
+        print("{:01d}{:01d}".format(tens_digit, units_digit), end=", ")
+
+print()
