fc393a158dbbc3487ecdd94e0648e44f9c234b05
VictorUdeme <snazzydo9@gmail.com>
2023-07-20 18:48:13 -0700

    Task 0003

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
-Last_digit = abs(number) % 10
-output = "Last digit of " + str(number) + " is " + str(Last_digit)
-if Last_digit > 5:
-    print (output, "and is greater than 5")
-elif Last_digit == 0:
-    print(output, "and is 0")
+
+# Get the last digit of the number using modulo operation (%)
+last_digit = abs(number) % 10
+
+print("The string Last digit of", number, "is", last_digit, end=" ")
+
+if last_digit > 5:
+    print("and is greater than 5")
+elif last_digit == 0:
+    print("and is 0")
 else:
-    print(output, "and is less than 6 and not 0")
+    print("and is less than 6 and not 0")
+
