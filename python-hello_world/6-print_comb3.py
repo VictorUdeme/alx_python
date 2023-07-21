f15154faf7d6fd22ab66c81f5fd1507a8675112b
VictorUdeme <snazzydo9@gmail.com>
2023-07-20 18:02:43 -0700

    Task 006

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,3 @@
 for tens_digit in range(0, 10):
     for units_digit in range(tens_digit + 1, 10):
-        print("{:01d}{:01d}".format(tens_digit, units_digit), end=", ")
-
-print()
+        print("{:01d}{:01d}".format(tens_digit, units_digit), end=", " if tens_digit < 8 else "\n")
