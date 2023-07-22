95987e140a022b200b929a240ca57eaf8e881613
VictorUdeme <snazzydo9@gmail.com>
2023-07-22 00:38:38 -0700

    Task 004

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -7,7 +7,7 @@ def fibonacci_sequence(n):
     fibonacci_numbers = [0, 1]
     while len(fibonacci_numbers) < n:
         next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]
-        if next_number <= 0:  # Check for integer overflow
+        if next_number <= 0: 
             return []
         fibonacci_numbers.append(next_number)
     
@@ -18,3 +18,4 @@ print(fibonacci_sequence(6))
 print(fibonacci_sequence(1))
 print(fibonacci_sequence(0))
 print(fibonacci_sequence(20))
+
