9965cbfd5513e461ebc15df0607c874384e481e8
VictorUdeme <snazzydo9@gmail.com>
2023-07-22 13:46:53 -0700

    Task 005

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,15 +1,12 @@
 def is_prime(number):
     if number <= 1:
         return False
-    
     for i in range(2, int(number ** 0.5) + 1):
         if number % i == 0:
             return False
-        
     return True
-    
-    is_prime = __import__('5-prime').is_prime
-print(is_prime(17))
-print(is_prime(15))
-print(is_prime(-5))
-print(is_prime(0))
+
+print(is_prime(17))  
+print(is_prime(15))  
+print(is_prime(-5))  
+print(is_prime(0))  
