d548e2e03e752050a0bb67f8abe299a0efd9d039
VictorUdeme <snazzydo9@gmail.com>
2023-07-21 23:21:18 -0700

    Task 00

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -5,7 +5,9 @@ def add(a, b):
         b = carry << 1
     return a
 
+if __name__ == "__main__":
+    add = __import__('0-sum').add
 
-print(add(1, 2))  
-print(add(98, 0))
-print(add(100, -2)) 
+    print(add(1, 2))  
+    print(add(98, 0))
+    print(add(100, -2)) 
