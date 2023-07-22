2c51cbf55bd3f55ec8d5448c4346f6c9ac5f4f28
VictorUdeme <snazzydo9@gmail.com>
2023-07-22 00:07:43 -0700

    Task 003

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,11 +1,15 @@
 def reverse_string(string):
+
     reversed_str = ""
+
+    
     for i in range(len(string) - 1, -1, -1):
-   
-      reversed_str += string[i]
+       
+        reversed_str += string[i]
+
     return reversed_str
 
-print(reverse_string("Hello"))
-print(reverse_string(""))
-print(reverse_string("madam"))
+print(reverse_string("Hello"))         
+print(reverse_string(""))             
+print(reverse_string("madam"))         
 print(reverse_string("Hello, World!"))
