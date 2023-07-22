a06c8c80cb4d6b610509c181c8440ab4eee29b37
VictorUdeme <snazzydo9@gmail.com>
2023-07-21 23:48:16 -0700

    Push 004

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -2,9 +2,8 @@ def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5 / 9
     return celsius
 
-if __name__ == "__main__":
-    convert_to_celsius = __import__('2-temperature').convert_to_celsius
+
 print(convert_to_celsius(100))
 print(convert_to_celsius(-40))
 print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32))
+print(convert_to_celsius(32)) 
--- python-functions/__pycache__/2-temperature.py
+++
@@ -1,9 +0,0 @@
-def convert_to_celsius(fahrenheit):
-    celsius = (fahrenheit - 32) * 5 / 9
-    return celsius
-
-convert_to_celsius = __import__('2-temperature').convert_to_celsius
-print(convert_to_celsius(100))
-print(convert_to_celsius(-40))
-print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32))
