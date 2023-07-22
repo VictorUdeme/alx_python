bfc65d8fb4514a75ba68823e8ec065c63f4902c4
VictorUdeme <snazzydo9@gmail.com>
2023-07-21 23:54:02 -0700

    Push task 004

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,15 +1,14 @@
-import math
-
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5 / 9
     return celsius
 
 def test_conversion(input_fahrenheit, expected_celsius):
     result = convert_to_celsius(input_fahrenheit)
-    assert math.isclose(result, expected_celsius, rel_tol=1e-9), f"Expected {expected_celsius}, but got {result}"
+    tolerance = 1e-9
+    if abs(result - expected_celsius) > tolerance:
+        raise AssertionError(f"Expected {expected_celsius}, but got {result}")
 
 test_conversion(100, 37.77777777777778)
 test_conversion(-40, -40.0)
 test_conversion(-459.67, -273.15)
 test_conversion(32, 0.0)
-
