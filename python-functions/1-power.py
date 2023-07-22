a9fdf9e35501a1d950eee0f0546d928bccb7358e
VictorUdeme <snazzydo9@gmail.com>
2023-07-21 23:16:11 -0700

    Task 001

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,20 +1,26 @@
+#!/usr/bin/python3
+
 def pow(a, b):
     if b == 0:
         return 1
-    
+
     result = 1
+    base = a
     exponent = abs(b)
 
     while exponent > 0:
         if exponent % 2 == 1:
-            result *= a
-        a *= a
+            result *= base
+        base *= base
         exponent //= 2
+
     return result if b >= 0 else 1 / result
 
-pow = __import__('1-power').pow
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5)) 
+if __name__ == "__main__":
+    pow = __import__('1-power').pow
+
+    print(pow(2, 2))
+    print(pow(98, 2))
+    print(pow(98, 0))
+    print(pow(100, -2))
+    print(pow(-4, 5)) 
