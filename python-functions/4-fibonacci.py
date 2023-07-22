b12597e363e0a19f40de7cee076b9ca9e05dc538
VictorUdeme <snazzydo9@gmail.com>
2023-07-22 00:32:56 -0700

    Push task 004

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -3,17 +3,16 @@ def fibonacci_sequence(n):
         return []
     elif n == 1:
         return [0]
-    elif n == 2:
-        return [0, 1]
     
     fibonacci_numbers = [0, 1]
-    for _ in range(2, n):
+    while len(fibonacci_numbers) < n:
         next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]
         if next_number <= 0:  # Check for integer overflow
             return []
         fibonacci_numbers.append(next_number)
     
     return fibonacci_numbers
+
 
 print(fibonacci_sequence(6))
 print(fibonacci_sequence(1))
