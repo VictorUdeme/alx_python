db8b20b3fa6c5439e954d4fc7472ecdb1005a312
VictorUdeme <snazzydo9@gmail.com>
2023-07-22 14:35:35 -0700

    Task 006

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,28 +1,33 @@
 def validate_password(password):
+    # Check length
     if len(password) < 8:
         return False
-    
+
+    # Check for uppercase, lowercase, and digit
     has_uppercase = False
     has_lowercase = False
     has_digit = False
 
-    for x in password:
-        if x.isupper():
+    for char in password:
+        if char.isupper():
             has_uppercase = True
-        elif x.islower():
+        elif char.islower():
             has_lowercase = True
-        elif x.isdigit():
+        elif char.isdigit():
             has_digit = True
-    
+
+    # Check for spaces
     if ' ' in password:
         return False
-    
+
+    # Check all conditions
     if has_uppercase and has_lowercase and has_digit:
         return True
     else:
         return False
-    validate_password = __import__('6-password').validate_password  
-print(validate_password("Password123"))
-print(validate_password("abc123"))
-print(validate_password("Password 123"))
-print(validate_password("password123"))   
+
+# Test cases
+print(validate_password("Password123"))   
+print(validate_password("abc123"))        
+print(validate_password("Password 123"))  
+print(validate_password("password123"))  
