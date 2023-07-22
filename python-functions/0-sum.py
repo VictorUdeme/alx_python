6f4b486aa8cd023b43511c19f910537ca218070f
VictorUdeme <snazzydo9@gmail.com>
2023-07-21 22:50:44 -0700

    Task  001

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3
 def add(a, b):
-   
     while b != 0:
         carry = a & b
         a = a ^ b
         b = carry << 1
     return a
 
-print(__import__('0-sum').add(1, 2))
-print(__import__('0-sum').add(98, 0))
-print(__import__('0-sum').add(100, -2))
- 
+
+print(__import__('0-sum').add(1, 2))    # Output: 3
+print(__import__('0-sum').add(98, 0))   # Output: 98
+print(__import__('0-sum').add(100, -2)) # Output: 98
