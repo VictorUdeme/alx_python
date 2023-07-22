8db32fe0defba3661e663998136fbba16362c334
VictorUdeme <snazzydo9@gmail.com>
2023-07-21 23:50:33 -0700

    push

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,9 +1,15 @@
+import math
+
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5 / 9
     return celsius
 
+def test_conversion(input_fahrenheit, expected_celsius):
+    result = convert_to_celsius(input_fahrenheit)
+    assert math.isclose(result, expected_celsius, rel_tol=1e-9), f"Expected {expected_celsius}, but got {result}"
 
-print(convert_to_celsius(100))
-print(convert_to_celsius(-40))
-print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32)) 
+test_conversion(100, 37.77777777777778)
+test_conversion(-40, -40.0)
+test_conversion(-459.67, -273.15)
+test_conversion(32, 0.0)
+
