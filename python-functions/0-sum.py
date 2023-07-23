81542112cf6ddbeab919bfe33c22d0a36591c8cb
VictorUdeme <snazzydo9@gmail.com>
2023-07-22 22:40:23 -0700

    Task 000

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,10 +1,6 @@
 def add(a, b):
-    while b != 0:
-        carry = a & b
-        a = a ^ b
-        b = carry << 1
+    for _ in range(b):
+        a += 1
+
     return a
-
-
-    add = __import__('0-sum').add
-
+add = __import__('0-sum').add
