b8ceb350ec3562b626597f8537fd4908938c67d6
VictorUdeme <snazzydo9@gmail.com>
2023-07-26 16:56:33 -0700

    Task003

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,6 +1,6 @@
 def best_score(a_dictionary):
-    """
-  A function thatReturns the key with the biggest integer value.
+  """
+  Functions that Returns the key with the biggest integer value.
 
   Arguments:
     a_dictionary: The dictionary to search.
@@ -8,11 +8,16 @@ def best_score(a_dictionary):
   Returns:
     The key with the biggest integer value, or None if no scores found.
   """
+
+  if a_dictionary is not None:
     best_score = None
     best_key = None
 
-    for key, value in  a_dictionary.items():
-        if best_score is None or value > best_score:
-            best_score = value
-            best_key = key
+    for key, value in a_dictionary.items():
+      if best_score is None or value > best_score:
+        best_score = value
+        best_key = key
+
     return best_key
+  else:
+    return None
