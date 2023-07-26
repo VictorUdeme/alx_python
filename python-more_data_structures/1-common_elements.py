4aab065c63ce29191c5b942a96a8cce814e5a569
VictorUdeme <snazzydo9@gmail.com>
2023-07-26 15:43:02 -0700

    Task 01

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,5 +1,7 @@
 def common_elements(set_1, set_2):
-    set_1 = {'Python', 'C', 'Javascript'}
-    set_2 = {'Bash', 'C', 'Ruby', 'Pearl'}
-    set_3 = set_1.union(set_2)
-    return(set_3)
+  """Returns a set of common elements in two sets."""
+  common_elements = set()
+  for element in set_1:
+    if element in set_2:
+      common_elements.add(element)
+  return common_elements
