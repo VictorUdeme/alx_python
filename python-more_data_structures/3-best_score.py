0cb4e0149cd606286748fdbe98f6b3a45f5ea4b6
VictorUdeme <snazzydo9@gmail.com>
2023-07-26 16:54:36 -0700

    Task 3

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -11,7 +11,7 @@ def best_score(a_dictionary):
     best_score = None
     best_key = None
 
-    for key, value in  a_dictionary:
+    for key, value in  a_dictionary.items():
         if best_score is None or value > best_score:
             best_score = value
             best_key = key
