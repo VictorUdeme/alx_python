f4927d016b8980d2af06826c344687773791df5d
VictorUdeme <snazzydo9@gmail.com>
2023-07-26 11:29:14 -0700

    Task 00

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -2,6 +2,8 @@ def no_c(my_string):
    if len(my_string) == 0:
         return my_string
    new_string = ""
+   if my_string[0] == "S":
+    new_string += "S"
    for character in my_string[1:]:
     if character != "c" and character != "C":
       new_string += character
