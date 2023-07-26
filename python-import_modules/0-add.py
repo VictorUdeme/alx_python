4d43a4d2db090fb244ccdefea77695b41b5d36c4
VictorUdeme <snazzydo9@gmail.com>
2023-07-25 21:23:34 -0700

    Task 000

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 a = 1
 b = 2
 
-# Import the 'add' function from add_0.py
-from add_0 import add
-
-# Print the result
-print("{} + {} = {}".format(a, b, add(a, b)))
+# The following block ensures the code is non-executable when imported
 if __name__ == "__main__":
-    pass
+    # Import the 'add' function from add_0.py
+    from add_0 import add
+
+   
+    print("{} + {} = {}".format(a, b, add(a, b)))
