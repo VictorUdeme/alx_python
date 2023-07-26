4d3fe3569380611e6d63825530404e62535067dc
VictorUdeme <snazzydo9@gmail.com>
2023-07-26 12:04:17 -0700

    Task 001 (Matrix)

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i, num in enumerate(row):
+        for i, num in enumerate(row): #Used the enumerate() function to keep track of index(i) while iterating 2ru loops
             print("{:d}".format(num), end=' ' if i < len(row) - 1 else '')
         print()
