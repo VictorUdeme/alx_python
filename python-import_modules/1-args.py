2986ff0caf1ddebdfe01b102cd3835745604fc68
VictorUdeme <snazzydo9@gmail.com>
2023-07-25 22:16:47 -0700

    Task 001

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -6,12 +6,8 @@ if __name__ == "__main__":
     num_arguments = len(sys.argv) - 1
 
     # Print the number of arguments
-    print("{} argument{}:".format(num_arguments, 's' if num_arguments != 1 else ''))
+    print("{} argument{}.".format(num_arguments, '' if num_arguments == 1 else 's'))
 
-    if num_arguments == 0:
-        # If no arguments were passed, print a dot and a new line
-        print(":.")
-    else:
-        # Otherwise, print the arguments with their positions
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print("{}: {}".format(i, arg))
+    # Print the list of arguments with their positions
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(i, arg))
