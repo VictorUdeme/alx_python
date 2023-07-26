3c6a89ba5421927012d7e61fbb18317b3779574e
VictorUdeme <snazzydo9@gmail.com>
2023-07-26 15:38:52 -0700

    Task 001

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,5 +1,5 @@
 def common_elements(set_1, set_2):
     set_1 = {'Python', 'C', 'Javascript'}
     set_2 = {'Bash', 'C', 'Ruby', 'Pearl'}
-    set_3 = set_1 | set_2
-    print(set_3)
+    set_3 = set_1.union(set_2)
+    return(set_3)
