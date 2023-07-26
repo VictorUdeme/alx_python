c4cc71fc5da3c843f0e64cedbe2481ee158e76af
VictorUdeme <snazzydo9@gmail.com>
2023-07-26 15:51:32 -0700

    Task 001

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,11 +1,2 @@
-def common_elements(*sets):
-  """Returns a set of common elements in two or more sets."""
-  common_elements = set()
-  for set_ in sets:
-    for element in set_:
-      if element in common_elements:
-        continue
-      if element in set_:
-        common_elements.add(element)
-  return common_elements
-
+def common_elements(set_1, set_2):
+    return set_1 & set_2
