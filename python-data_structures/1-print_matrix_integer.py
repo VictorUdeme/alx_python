5ca16720429488a98b979d1e63b31c72651ab58d
VictorUdeme <snazzydo9@gmail.com>
2023-07-26 11:57:42 -0700

    Lists-Lists Task 001

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), ends= ' ' )
-    print()
+        for i, num in enumerate(row):
+            print("{:d}".format(num), end=' ' if i < len(row) - 1 else '')
+        print()
