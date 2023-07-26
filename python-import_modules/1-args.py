34bdb7ae749737e27149f5a6d42e006bfde537bc
VictorUdeme <snazzydo9@gmail.com>
2023-07-26 10:31:44 -0700

    make a script dynamic Task

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -4,7 +4,7 @@ def print_arguments():
     argv = sys.argv[1:]
     num_args = len(argv)
 
-    print(f"{num_args} argument{'s' if num_args != 1 else ''}: {'. ' if num_args == 0 else ''}")
+    print(f"{num_args} argument{'s' if num_args != 1 else ''}: {'' if num_args == 0 else ':'}")
 
     if num_args > 0:
         for i, arg in enumerate(argv, 1):
