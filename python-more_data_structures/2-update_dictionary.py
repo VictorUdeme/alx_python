643263cc88a5c26e539959e8e233bfee03d5971e
VictorUdeme <snazzydo9@gmail.com>
2023-07-26 16:45:55 -0700

    Task 002

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,8 +1,19 @@
-def print_sorted_dictionary(a_dictionary):
-    if a_dictionary is None:
-        return
+def update_dictionary(a_dictionary, key, value):
+  """
+  Replaces or adds key/value in a dictionary.
 
-    keys = sorted(a_dictionary.keys())
-    for key in keys:
-        value = a_dictionary[key]
-        print(f"{key}: {value}")
+  Args:
+    a_dictionary: The dictionary to update.
+    key: The key to update or add.
+    value: The value to associate with the key.
+
+  Returns:
+    The updated dictionary.
+  """
+
+  if key in a_dictionary:
+    a_dictionary[key] = value
+  else:
+    a_dictionary[key] = value
+
+  return a_dictionary
