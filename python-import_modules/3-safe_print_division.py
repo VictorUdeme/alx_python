e4ef8f398d08751e88bc604bb6169b7508a43cde
VictorUdeme <snazzydo9@gmail.com>
2023-07-25 22:43:13 -0700

    Task 003

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -7,8 +7,10 @@ def safe_print_division(a, b):
     finally:
         print("Inside result:".format(result))
 
-   
-    safe_print_division(a, b)
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}.format(a, b, result)")
         
     
     
