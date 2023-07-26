c1c0838e73cd5a520abe717a42d0bc55cb247093
VictorUdeme <snazzydo9@gmail.com>
2023-07-25 21:46:03 -0700

    Task 000

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,17 +1,11 @@
-# File: main.py
 
 a = 1
 b = 2
 
-# Import the 'add_0' module
-import add_0
-
-# Calculate the result using the 'add' function from 'add_0' module
-result = add_0.add(a, b)
-
-# Print the result using .format() to display the values
-print("{a} + {b} = {result}".format(a=a, b=b, result=result))
-
 if __name__ == "__main__":
-    pass
 
+    from add_0 import add
+
+    result = add(a, b)
+
+    print("{} + {} = {}".format(a, b, result))
