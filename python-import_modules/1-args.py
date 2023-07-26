256ec431233cb9a7251f1bfe679ce7c4eae00720
VictorUdeme <snazzydo9@gmail.com>
2023-07-25 22:18:19 -0700

    Task 001

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,13 +1,17 @@
 import sys
 
-# The following block ensures the code is non-executable when imported
+
 if __name__ == "__main__":
-    # Get the number of arguments
-    num_arguments = len(sys.argv) - 1
+    
+    numbers = len(sys.argv) - 1
 
     # Print the number of arguments
-    print("{} argument{}.".format(num_arguments, '' if num_arguments == 1 else 's'))
+    print("{} argument{}:".format(numbers, 's' if numbers != 1 else ''))
 
-    # Print the list of arguments with their positions
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(i, arg))
+    if numbers == 0:
+        # If no arguments were passed, print a dot and a new line
+        print(":.")
+    else:
+        # Otherwise, print the arguments with their positions
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print("{}: {}".format(i, arg))
