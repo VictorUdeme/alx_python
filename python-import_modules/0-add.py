37e5fc9878f5e139c01e186be97eff6efc689a7c
VictorUdeme <snazzydo9@gmail.com>
2023-07-25 21:18:23 -0700

    Task 000

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,8 @@
-import add_0
-
 a = 1
 b = 2
-if __name__ == "__main__":
-    from add_0 import add
-print(f"{a} + {b} = {add_0.add(a, b)}")
+
+# Import the 'add' function from add_0.py
+from add_0 import add
+
+# Print the result
+print("{} + {} = {}".format(a, b, add(a, b)))
