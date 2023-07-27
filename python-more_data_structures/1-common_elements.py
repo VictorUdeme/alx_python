d62e959e8e2cec21af56bf8510a1c1fc4f92f704
VictorUdeme <snazzydo9@gmail.com>
2023-07-26 21:35:11 -0700

    Task 001

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,12 +1,2 @@
 def common_elements(set_1, set_2):
-    # Initialize an empty set to store the common elements
-    common_set = set()
-
-    # Iterate through each element in set_1
-    for element in set_1:
-        # Check if the element is present in set_2
-        if element in set_2:
-            # If it's present, add it to the common_set
-            common_set.add(element)
-
-    return common_set
+    return set_1.intersection(set_2)
