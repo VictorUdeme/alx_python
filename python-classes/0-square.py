fcba248f63ef124b1524aea230373928ea2b5ebc
VictorUdeme <snazzydo9@gmail.com>
2023-08-03 09:29:50 -0700

    Task 000

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,11 @@
+"""
+task 0, classes project
+"""
 class Square:
     """
-This class defines a square by:
+    This class defines a square by:
 
-* Private instance attribute: size
-* Instantiation with size (no type/value verification)
-
-You are not allowed to import any module.
-"""
+    """
 
     def __init__(self, size):
         self.__size = size
