b0b386e929a5524d843d09c1c7487ce63b7a893d
VictorUdeme <snazzydo9@gmail.com>
2023-08-03 09:35:03 -0700

    Task 001

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,14 +1,17 @@
+"""
+task 0, classes project
+"""
 class Square:
     """
 This class defines a square by:
-
-* Private instance attribute: size
 * Instantiation with optional size: def __init__(self, size=0)
 
-You are not allowed to import any module.
-"""
+    """
 
     def __init__(self, size=0):
+        '''* Private instance attribute: size
+        * Instantiation with optional size: def __init__(self, size=0)
+        '''
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
