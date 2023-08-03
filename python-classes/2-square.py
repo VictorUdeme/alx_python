9e3768245ecebb08e3bdc9b30ba2ae5f2873f98e
VictorUdeme <snazzydo9@gmail.com>
2023-08-03 09:40:33 -0700

    Task 002

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,22 +1,31 @@
+"""
+task 2, classes project
+"""
 class Square:
     """
 This class defines a square by:
 
-* Private instance attribute: size
-* Instantiation with optional size (default 0)
-* Size must be an integer, otherwise raise a TypeError exception with the message "size must be an integer"
-* If size is less than 0, raise a ValueError exception with the message "size must be >= 0"
-* Public instance method: def area(self): that returns the current square area
+
+
 
 """
 
     def __init__(self, size=0):
+        """* Private instance attribute: size
+           * Instantiation with optional size (default 0)
+        """
         if not isinstance(size, int):
+            """  If size is less than 0, raise a ValueError exception
+                 with the message "size must be >= 0
+            """
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
+        """Public instance method:
+           def area(self): that returns the current square area
+        """
         return self.__size * self.__size
 
