e68fdfa93b9f5c6c548f0fe7266201fe77da1472
VictorUdeme <snazzydo9@gmail.com>
2023-08-03 19:38:05 -0700

    Task 002

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -3,13 +3,6 @@ task 2, inheritance Task
 """
 def inherits_from(obj, a_class):
     """
-    Check if the object is an instance of a class that inherited (directly or indirectly) from the specified class.
-
-    Parameters:
-        obj: Any - The object to check.
-        a_class: type - The specified class to compare against.
-
-    Returns:
-        bool: True if the object is an instance of a subclass of the specified class; otherwise, False.
+   Good
     """
     return issubclass(obj.__class__, a_class) and obj.__class__ is not a_class
