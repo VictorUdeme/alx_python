78293e31294affa9f43de50fa2b6156cbba897a0
VictorUdeme <snazzydo9@gmail.com>
2023-08-03 19:29:50 -0700

    Task 001

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,21 +1,19 @@
 """
-task 1, inheritance Task
+This function takes two arguments: obj,
+ which is the object you want to check, 
+ and a_class, which is the specified class you want to compare against. 
+ It will return True if obj is exactly an instance of the specified a_class,
+ otherwise, it will return False.
 """
-
 def is_kind_of_class(obj, a_class):
-    """Returns True if the object is an instance of, 
-    or if the object is an instance of a class that 
-    inherited from, the specified class ; otherwise False.
     """
-    # Check if the object is an instance of the specified class.
-    if isinstance(obj, a_class):
-     return True
-    # Check if the object is an instance of a class that inherited from the specified class.
-    while hasattr(a_class, '__bases__'):
-     if isinstance(obj, a_class.__bases__):
-      return True
-     a_class = a_class.__bases__[0]
-    # If the object is not an instance of the specified class or any of its subclasses, return False.
-    return False
+    Check if the object is an instance of, or if it's an instance of a class that inherited from, the specified class.
 
+    Parameters:
+        obj: Any - The object to check.
+        a_class: type - The specified class to compare against.
 
+    Returns:
+        bool: True if the object is an instance of the specified class or its subclass; otherwise, False.
+    """
+    return isinstance(obj, a_class)
