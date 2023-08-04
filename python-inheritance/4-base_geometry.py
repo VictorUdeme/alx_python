cdce68c3ce537431df1535110d4dd181a5be7ebf
VictorUdeme <snazzydo9@gmail.com>
2023-08-03 18:27:15 -0700

    Task 004

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,6 +1,7 @@
 """BaseGeometry class"""
 class BaseGeometry:
     def area(self):
+        """This raises an Exception error if area is not implemented"""
         raise Exception("area() is not implemented")
 
 
