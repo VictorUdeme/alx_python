676b00298b6837d317543609bd69df7e3b30ea38
VictorUdeme <snazzydo9@gmail.com>
2023-08-03 19:15:13 -0700

    Task 008

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -66,13 +66,14 @@ class Square(Rectangle):
             Here, The __init__() method of the Square class takes a single parameter: size.
 
         """
-        self.integer_validator("size", size)
         super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+       
 
     def area(self):
         """
             Implement the area method here as we did the previous.
         """
         return self.__size * self.__size
-
-        
+   
