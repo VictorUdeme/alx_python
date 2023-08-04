38880ac41cf4580bde0816df37c680aed3c4dd41
VictorUdeme <snazzydo9@gmail.com>
2023-08-03 19:39:48 -0700

    Task 002

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,8 +1,7 @@
 """
 task 2, inheritance Task
 """
+
+  # Good
 def inherits_from(obj, a_class):
-    """
-   Good
-    """
     return issubclass(obj.__class__, a_class) and obj.__class__ is not a_class
