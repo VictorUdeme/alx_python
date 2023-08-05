d02a606356552e03d2e7c43bb39d3eb227ad9369
VictorUdeme <snazzydo9@gmail.com>
2023-08-04 22:28:52 -0700

    Task 002

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,5 +1,10 @@
-"""This functions takes two parameters"""
+"""
+This functions takes 
+two parameters
+
+"""
 def inherits_from(obj, a_class):
-    """Good"""
+    
+    #Good
     return (issubclass(obj.__class__, a_class) and \
             obj.__class__ is not a_class)
