96d57c399c9340f9011ab945a736d242a61c0479
VictorUdeme <snazzydo9@gmail.com>
2023-08-05 13:19:06 -0700

    Task 002

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -5,14 +5,12 @@ This is a module that contains a function to check if an object inherits from a 
 
 def inherits_from(obj, a_class):
     """
-    Check if an object inherits from a given class.
-
-    Parameters:
-        obj (object): The object to check.
-        a_class (class): The class to check against.
-
-    Returns:
-        bool: True if the object inherits from the class, False otherwise.
+    description for the method(function)
     """
-    return issubclass(obj.__class__, a_class) and \
-           obj.__class__ is not a_class
+    
+    obj_class = type(obj)
+    if issubclass(obj_class, a_class):
+        return True
+    else:
+        return False
+
