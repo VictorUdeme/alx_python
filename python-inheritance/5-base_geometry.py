4755531f7d7f63904093141c044419ea42d57fd5
VictorUdeme <snazzydo9@gmail.com>
2023-08-05 13:32:59 -0700

    Task 005

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,8 +1,34 @@
-"""BaseGeometry class (Integer_Validator)"""
-class BaseGeometry:
+"""
+BaseGeometry class
+"""
+
+class BaseGeometryMeta(type):
+     """
+     Meta class for BaseGeometry
+     """
+     def __dir__(self):
+        # Call the parent class's __dir__ method to get the default attributes
+       attributes = super().__dir__()
+       new_attribute = [item for item in attributes if item !="__init_subclass__"]
+       return new_attribute
+    
+
+
+class BaseGeometry(metaclass=BaseGeometryMeta):
+    """
+    This class serves as a foundation for creating specific geometrical shape classes
+    with common attributes and methods related to geometry.
+    """
+    def __dir__(self):
+        # Call the parent class's __dir__ method to get the default attributes
+       attributes = super().__dir__()
+       new_attribute = [item for item in attributes if item !="__init_subclass__"]
+       return new_attribute
+    
     def area(self):
         """This raises an Exception error if area is not implemented"""
         raise Exception("area() is not implemented")
+
 
     def integer_validator(self, name, value):
         """This method validates the value passed in.
@@ -17,7 +43,3 @@ class BaseGeometry:
             
             raise ValueError(f"{name} must be greater than 0")
 
-
-"""instanciation"""
-myobject = BaseGeometry()
-print(dir(myobject))
