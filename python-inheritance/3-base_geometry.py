67c32cb9b1815b83a1d8e44eb70292b379418a64
VictorUdeme <snazzydo9@gmail.com>
2023-08-05 13:10:16 -0700

    Task 003

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -2,8 +2,19 @@
 BaseGeometry class
 """
 
+class BaseGeometryMeta(type):
+     """
+     Meta class for BaseGeometry
+     """
+     def __dir__(self):
+        # Call the parent class's __dir__ method to get the default attributes
+       attributes = super().__dir__()
+       new_attribute = [item for item in attributes if item !="__init_subclass__"]
+       return new_attribute
+    
 
-class BaseGeometry:
+
+class BaseGeometry(metaclass=BaseGeometryMeta):
     """
     This class serves as a foundation for creating specific geometrical shape classes
     with common attributes and methods related to geometry.
