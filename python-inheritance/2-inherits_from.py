547bd32d449965bd1abcbf18df8c7c4b0a4b0e61
VictorUdeme <snazzydo9@gmail.com>
2023-08-05 13:22:25 -0700

    Task 002

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -9,7 +9,7 @@ def inherits_from(obj, a_class):
     """
     
     obj_class = type(obj)
-    if issubclass(obj_class, a_class):
+    if issubclass(obj_class, a_class) and obj_class is not a_class:
         return True
     else:
         return False
