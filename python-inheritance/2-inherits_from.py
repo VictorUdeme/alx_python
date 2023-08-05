5d5119c0b0abc402cf284cbb2ab72f52598b6fa7
VictorUdeme <snazzydo9@gmail.com>
2023-08-04 22:45:48 -0700

    PEP

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -2,6 +2,7 @@
 This is a module that contains a function to check if an object inherits from a given class.
 """
 
+
 def inherits_from(obj, a_class):
     """
     Check if an object inherits from a given class.
@@ -13,4 +14,6 @@ def inherits_from(obj, a_class):
     Returns:
         bool: True if the object inherits from the class, False otherwise.
     """
-    return issubclass(obj.__class__, a_class) and obj.__class__ is not a_class
+    return (
+        issubclass(obj.__class__, a_class) and obj.__class__ is not a_class
+    )
