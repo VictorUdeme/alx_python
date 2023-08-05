53cd49ebeb3ebdcce5e28a30ff166c67f92346d2
VictorUdeme <snazzydo9@gmail.com>
2023-08-04 22:30:27 -0700

    Task 002

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,10 +1,11 @@
 """
 This functions takes 
 two parameters
-
 """
 def inherits_from(obj, a_class):
     
-    #Good
+    """
+    Description of the function
+    """
     return (issubclass(obj.__class__, a_class) and \
             obj.__class__ is not a_class)
