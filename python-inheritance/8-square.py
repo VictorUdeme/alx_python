ac4b79f07b522ba27288ebe06490352fdb56758b
VictorUdeme <snazzydo9@gmail.com>
2023-08-05 20:56:42 -0700

    Task 008

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,5 +1,18 @@
 """BaseGeometry class(SuperClass)"""
-class BaseGeometry:
+
+
+class BaseGeometryMeta(type):
+     """
+     Meta class for BaseGeometry
+     """
+     def __dir__(self):
+        # Call the parent class's __dir__ method to get the default attributes
+       attributes = super().__dir__()
+       new_attribute = [item for item in attributes if item !="__init_subclass__"]
+       return new_attribute
+
+
+class BaseGeometry(metaclass=BaseGeometryMeta):
     def __dir__(self):
         # Call the parent class's __dir__ method to get the default attributes
        attributes = super().__dir__()
