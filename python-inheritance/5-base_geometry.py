c9b04aec4d5c5cdab797495a1539d27bcd6394f5
VictorUdeme <snazzydo9@gmail.com>
2023-08-07 20:31:02 -0700

    Task 005

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -9,8 +9,8 @@ class BaseGeometryMeta(type):
      def __dir__(self):
         # Call the parent class's __dir__ method to get the default attributes
        attributes = super().__dir__()
-       new_attribute = [item for item in attributes if item !="__init_subclass__"]
-       return new_attribute
+       return[item for item in attributes if item !="__init_subclass__"]
+       
     
 
 
@@ -19,11 +19,11 @@ class BaseGeometry(metaclass=BaseGeometryMeta):
     This class serves as a foundation for creating specific geometrical shape classes
     with common attributes and methods related to geometry.
     """
-    def __dir__(self):
+    def __dir__(cls):
         # Call the parent class's __dir__ method to get the default attributes
        attributes = super().__dir__()
-       new_attribute = [item for item in attributes if item !="__init_subclass__"]
-       return new_attribute
+       return[item for item in attributes if item !="__init_subclass__"]
+    
     
     def area(self):
         """This raises an Exception error if area is not implemented"""
