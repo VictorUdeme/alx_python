3406d118bbd036ed17b48be786747fce0d331b38
VictorUdeme <snazzydo9@gmail.com>
2023-08-08 14:22:54 -0700

    Task 008

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,76 +1,53 @@
 """
-Meta class for BaseGeometry
+BaseGeometry class
 """
 
 
-class BaseGeometryMeta(type):
-    def __dir__(cls):
-        # Call the parent class's __dir__ method to get the default attributes
-        attributes = super().__dir__()
-        new_attributes = [item for item in attributes
-                          if item != "__init_subclass__"]
-        return new_attributes
-
-
-class BaseGeometry(metaclass=BaseGeometryMeta):
-    def __dir__(cls):
-        # Call the parent class's __dir__ method to get the default attributes
-        attributes = super().__dir__()
-        new_attributes = [item for item in attributes if item !=
-                          "__init_subclass__"]
-        return new_attributes
-
-    def area(self):
-        """
-        This raises an Exception error if area is not implemented
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        This method validates the value passed in.
-        """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
+BaseGeometry = __import__('5-base_geometry').BaseGeometry 
 class Rectangle(BaseGeometry):
+   
+    """
+        This class inherits from the BaseGeometry class and
+        adds two private attributes: __width and __height
+    """
     def __dir__(cls):
-        attributes = super().__dir__()
-        new_attributes = [item for item in attributes
-                          if item != "__init_subclass__"]
-        return new_attributes
-
+        # Call the parent class's __dir__ method to get the default attributes
+       attributes = super().__dir__()
+       return[item for item in attributes if item !="__init_subclass__"]
+    
     def __init__(self, width, height):
-        """The init method"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+        """
+            The __init__() method of this subclass validate the 
+            width and height perimeters
+        """
+        self.__width = super().integer_validator("width", width)
+        self.__height = super().integer_validator("height", height)
+
 
     def area(self):
         """
-        Calculates the area of the Rectangle
+            This Calculate the area of the rectangle
+            The area of a rectangle is calculated by: 
+            multiplying the Width and Height
         """
         return self.__width * self.__height
-
+    
     def __str__(self):
         """
-        The Str dunder method
+        The __str__() method of the Rectangle class returns a string representation of the rectangle.
+        it follows this format:
+            [Rectangle] <width>/<height>
         """
         return f"[Rectangle] {self.__width}/{self.__height}"
-
     def __repr__(self):
         """
-        This returns a representation of the rectangle that can
+        The __repr__() method of the Rectangle class returns a representation of the rectangle that can 
         be used to recreate the object.
         """
-        return f"Rectangle({self.__width}, {self.__height})"
+        return f"Rectangle{self.__width}, {self.__height}"
+        
 
-
+Rectangle = __import__('6-rectangle').Rectangle
 class Square(Rectangle):
     """
     This class inherits from the Rectangle class.
@@ -90,3 +67,4 @@ class Square(Rectangle):
         Implement the area method here as we did the previous.
         """
         return self.__size * self.__size
+     
