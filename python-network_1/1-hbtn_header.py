9f40fb6e16842ab51aebd224118b641e6c102e06
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 14:43:57 -0700

    Web_Scrap[1]

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -10,15 +10,19 @@ def get_x_request_id(url):
     from the response to the given URL.
     """
     response = get_x_request_id
+    x_request_id = response.header.get('X-Request-Id')
+    return x_request_id
 
 if __name__ == "__main__":
     if len(sys.argv) !=2:
-        print("Usage: python scropt_name.py <URL>")
+        print("Usage: python script_name.py <URL>")
     else:
         ur1 = sys.argv[1]
         x_request_id = get_x_request_id(ur1)
 
         if x_request_id:
-            print("X-Request-Id:", x_request_id)
+            print(x_request_id)
         else:
-            print("X-Reuest-Id not found in response headers")
+            print("X-Request-Id not found in response headers")
+
+
