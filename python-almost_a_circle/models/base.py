f98986061d3271d3df7ceba08bd2d8c6edc95b7c
VictorUdeme <snazzydo9@gmail.com>
2023-08-09 22:29:33 -0700

    Base001

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,19 @@
+"""
+This defines BaseClass which would
+be used for all future purpose.
+"""
+
+
 class Base:
+    """
+    This class keeps track of the number of instances created and assigns unique ids to them.
+    """
+
     __nb_objects = 0
     def __init__(self, id=None):
+        """
+        Initializes a new instance of the Base class.
+        """
         if id is not None:
             self.id = id
         else:
