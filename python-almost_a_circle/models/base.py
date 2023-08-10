b9547ff6d63136190f2c95af1f659131325eeff0
VictorUdeme <snazzydo9@gmail.com>
2023-08-09 23:12:24 -0700

    TaskBase_base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -15,5 +15,6 @@ class Base:
         if id is not None:
             self.id = id
         else:
+            """Increment __nb_objects"""
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
