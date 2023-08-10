a3b3bc5511661d81df0e93a961f0e81f0b65fcd7
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 00:48:59 -0700

    square(Rec)

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,7 +4,7 @@ Importing from rectangle_class
 
 
 from models.rectangle import Rectangle
-class Square:
+class Square(Rectangle):
     """Square class"""
     def __init__(self, size, x=0, y=0, id=None):
         """
@@ -28,4 +28,4 @@ class Square:
         """
         Overloading str
         """
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
