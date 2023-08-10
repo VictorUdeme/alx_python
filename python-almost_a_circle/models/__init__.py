ba1d6776cfbd5b32f6390da56fb4b641211d3a87
VictorUdeme <snazzydo9@gmail.com>
2023-08-09 22:47:30 -0700

    init_000

--- python-almost_a_circle/models/__init__.py
+++ python-almost_a_circle/models/__init__.py
@@ -7,8 +7,3 @@ from models.base import Base
 Creating an instance of Base without passing an id
 """
 instance_without_id = Base()
-
-"""
-Accessing the id attribute of the instance
-"""
-print("OK")
