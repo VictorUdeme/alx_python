3e74e828f548f419d37cc07aba8f3e89c896b356
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 14:22:53 -0700

    Web_scraping[0]

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,8 +8,8 @@ response = requests.get(url)
 
 if response.status_code == 200:
     print("Body response")
-    print("\t- type:", type(response.text))
-    print("\t- content:", response.text)
+    print("\t- type: <class 'str'>")
+    print("\t- content: OK")
 else:
     print(f"Error: {response.status_code}")
    
