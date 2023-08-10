c070fb20872971615a3598a334e3229199955928
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 01:19:42 -0700

    Task 00(http)

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -11,7 +11,3 @@ def get_x_request_id(url):
     """
     response = requests.get(url)
     return response.headers['X-Request-Id']
-
-if __name__ == '__main__':
-    url = sys.argv[1]
-    print(get_x_request_id(url))
