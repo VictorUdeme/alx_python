d8c2983733fffe7512e0c1afa97db65eb02490e3
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 01:28:48 -0700

    Task 00(0)

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,4 +7,8 @@ def fetch_status():
     defines fetch_status
     """
     response = requests.get('https://alu-intranet.hbtn.io/status')
-    return response.text
+    if response.status_code == 200:
+        return response.text
+    else:
+        raise Exception('Failed to fetch status code')
+    
