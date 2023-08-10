6fc415477d3de5f87954fdbb1585fab4ba6041db
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 14:32:45 -0700

    Web_Scraping[1]

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -9,5 +9,16 @@ def get_x_request_id(url):
     Gets the value of the X-Request-Id header
     from the response to the given URL.
     """
-    response = requests.get(url)
-    return response.headers['X-Request-Id']
+    response = get_x_request_id
+
+if __name__ == "__main__":
+    if len(sys.argv) !=2:
+        print("Usage: python scropt_name.py <URL>")
+    else:
+        ur1 = sys.argv[1]
+        x_request_id = get_x_request_id(ur1)
+
+        if x_request_id:
+            print("X-Request-Id:", x_request_id)
+        else:
+            print("X-Reuest-Id not found in response headers")
