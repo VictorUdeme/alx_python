855d08c9736233e9c0f9bb9ec818f57b4048c8bf
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 00:40:25 -0700

    Task_Square(rectangle)

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -12,6 +12,18 @@ class Square:
         """
         super().__init__(size, size, x, y, id)
 
+    """
+    Getter and setter for width
+    """
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """
         Overloading str
