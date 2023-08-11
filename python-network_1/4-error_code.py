accde950dc9ba8754b9d62450469310020cedfda
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 20:08:16 -0700

    Web_Scrap[3]

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -6,15 +6,11 @@ module-code_erro
 import requests
 import sys
 
-def send_request(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            print(response.text)
-        else:
-            print("Error code:", response.status_code)
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
+def get_url_content(url):
+    response = requests.get(url)
+    status_code = response.status_code
 
-# Replace 'url_here' with the actual URL you want to send a request to
-send_request('url_here')
+    if status_code >= 400:
+        print(f"Error code: {status_code}")
+    else:
+        print(response.text)
