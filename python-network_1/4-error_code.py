85bb760b02179223aad234cdf47e30e34e8897c4
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 19:30:20 -0700

    Task_4

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -7,24 +7,15 @@ import requests
 import sys
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <URL> <email>")
-        sys.exit(1)
-
     url = sys.argv[1]
-    email = sys.argv[2]
 
-    data = {"email": email}
+    response = requests.get(url)
 
-    response = requests.post(url, data=data)
-
-    if response.status_code == 200:
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
         print("Response Body:")
         print(response.text)
-    else:
-        print("Error:", response.status_code)
 
 if __name__ == "__main__":
     main()
-
-
