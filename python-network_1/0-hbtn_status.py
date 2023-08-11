8e24eadf61d4aaf4b4158a92df26928e322a7e59
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 19:00:04 -0700

    Task 00

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,14 @@
-"""
-imports module
-"""
+"""importing modules"""
+
 import requests
 
-response = requests.get('https://alu-intranet.hbtn.io/status')
+url = "https://alu-intranet.hbtn.io/status"
+response = requests.get(url)
 
+if response.status_code == 200:
+    data = response.json()
+    print("Body response:")
+    print("\t- type:", type(data))
+    print("\t- content:", data)
+else:
+    print(f"Error: Unable to fetch data. Status code: {response.status_code}")
