06ca71b30016127aa63fe01d617cbff327741784
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 20:52:52 -0700

    Web_Scrap[0]

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,14 +4,5 @@ importing module
 
 
 import requests
-
-url = "https://alu-intranet.hbtn.io/status"
-response = requests.get(url)
-
-if response.status_code == 200:
-    content = response.text
-    print("[{}]".format(content))
-    print("({} chars long)".format(len(content)))
-else:
-    print("Error:", response.status_code)
+response = requests.get("https://alu-intranet.hbtn.io/status")
  
