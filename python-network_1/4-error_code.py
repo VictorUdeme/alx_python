9d799cdf2745049b6a2bca54d800618378c9905e
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 20:15:41 -0700

    Error[3]

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -7,14 +7,21 @@ import requests
 import sys
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <URL>")
+        sys.exit(1)
+    
     url = sys.argv[1]
-    response = requests.get(url)
-
-    if response.status_code >= 400:
-        print("Error code:", response.status_code)
-    else:
+    
+    try:
+        response = requests.get(url)
         print(response.text)
-
+        
+        if response.status_code >= 400:
+            print(f"Error code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+    
 if __name__ == "__main__":
     main()
 
