b82ce898dc7ddd7745d4b2db3a33c644ebd235b1
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 18:57:03 -0700

    Task 00

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,10 +7,3 @@ url = 'https://alu-intranet.hbtn.io/status'
 
 response = requests.get(url)
 
-if response.status_code == 200:
-    print('Body of the response:')
-    print('-' * 10)
-    print(response.text)
-else:
-    print('Error fetching the status page')
-    print(response.status_code)
