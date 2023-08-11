542fe47d6fb67f3ae92bff1ec1508186c78d7577
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 20:13:35 -0700

    Task[Push]

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -6,18 +6,15 @@ module-code_erro
 import requests
 import sys
 
-def get_url_content(url):
+def main():
+    url = sys.argv[1]
     response = requests.get(url)
-    status_code = response.status_code
 
-    if status_code >= 400:
-        print(f"Error code: {status_code}")
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
     else:
         print(response.text)
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
-        get_url_content(url)
-    else:
-        print("Please provide a URL as an argument.")
+    main()
+
