e34c2a77242d17686cb76dbfcabc54a02a964f90
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 19:09:14 -0700

    Task 001

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -6,15 +6,21 @@ import module(requests and sys)
 import requests
 import sys
 
+if len(sys.argv) < 2:
+    print("Usage: python script_name.py <URL>")
+    sys.exit(1)
+
 url = sys.argv[1]
-
 response = requests.get(url)
-headers = response.headers
 
-if 'X-Request-Id' in headers:
-    x_request_id = headers['X-Request-Id']
-    print("X-Request-Id:", x_request_id)
+if response.status_code == 200:
+    x_request_id = response.headers.get('X-Request-Id')
+    if x_request_id:
+        print("X-Request-Id:", x_request_id)
+    else:
+        print("X-Request-Id not found in response headers.")
 else:
-    print("X-Request-Id not found in response headers.")
+    print(f"Error: Unable to fetch data. Status code: {response.status_code}")
+
 
     
