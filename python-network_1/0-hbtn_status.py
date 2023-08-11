cb7acca8264f924c6a1f7e74eddf8d57cf7df6b8
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 18:40:57 -0700

    WS0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,10 +6,3 @@ import requests
 url = 'https://alu-intranet.hbtn.io/status'
 response = requests.get(url)
 
-if response.status_code == 200:
-    print("Body response")
-    print("\t- type: <class 'str'>")
-    print("\t- content: OK")
-else:
-    print(f"Error: {response.status_code}")
-   
