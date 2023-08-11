8a80f8eff52e1fe12336fbfd0d0eaef27e9c3011
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 18:55:10 -0700

    Task 00

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,35 +1,16 @@
 """
 imports module
 """
-
-
 import requests
 
-def fetch_and_display_status(url):
-    """
-    Fetches the status from a given URL using the requests package.
+url = 'https://alu-intranet.hbtn.io/status'
 
-    Args:
-        url (str): The URL to fetch the status from.
+response = requests.get(url)
 
-    Returns:
-        None
-
-    Prints:
-        The type and content of the response in the expected format.
-    """
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        data = response.text  # Get the response content as text
-        print("Body response:")
-        print("\t- type:", type(data))
-        print("\t- content:", data)
-    else:
-        print("Failed to fetch the URL. Status code:", response.status_code)
-
-# URL to fetch the status from
-url = "https://alu-intranet.hbtn.io/status"
-
-# Fetch and display the status
-fetch_and_display_status(url)
+if response.status_code == 200:
+    print('Body of the response:')
+    print('-' * 10)
+    print(response.text)
+else:
+    print('Error fetching the status page')
+    print(response.status_code)
