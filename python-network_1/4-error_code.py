b614e1ee80a035301d661b0d68ede08751a15578
VictorUdeme <snazzydo9@gmail.com>
2023-08-11 00:12:31 -0700

    Web[Error]

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -6,18 +6,22 @@ module-code_erro
 import requests
 import sys
 
-url = input("Enter a URL: ").strip()
+def fetch_and_display(url):
+    response = requests.get(url)
+    
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
 
-if not url:
-    print("Error: URL is required")
-    sys.exit()
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
+    
+    url = sys.argv[1]
+    fetch_and_display(url)
 
-response = requests.get(url)
 
-if response.status_code >= 400:
-    print(f"Error code: {response.status_code}")
-    sys.exit()
-
-print(response.text)
 
 
