1ded3a950d4826a6b63313fa8fed8ef54d068a82
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 18:43:30 -0700

    TASK 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,5 +3,13 @@ imports requests_package
 """
 import requests
 
-url = 'https://alu-intranet.hbtn.io/status'
+url = "https://alu-intranet.hbtn.io/status"
 response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    print("Body response:")
+    print("\t- type:", type(data))
+    print("\t- content:", data)
+else:
+    print("Failed to fetch the URL. Status code:", response.status_code)
