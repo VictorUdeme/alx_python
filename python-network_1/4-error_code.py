814765c34d6495184b39526a96211bf1a327584f
VictorUdeme <snazzydo9@gmail.com>
2023-08-11 00:04:07 -0700

    Web_Scrap[Error}

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -6,20 +6,14 @@ module-code_erro
 import requests
 import sys
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py <URL>")
-        sys.exit(1)
-    
-    url = sys.argv[1]
-    
-    try:
-        response = requests.get(url)
-        if response.status_code >= 400:
-            print(f"Error code: {response.status_code}")
-    except requests.exceptions.RequestException:
-        print("An error occurred")
+url = input("Enter a URL: ")
 
-if __name__ == "__main__":
-    main()
+response = requests.get(url)
+
+if response.status_code >= 400:
+    print(f"Error code: {response.status_code}")
+    sys.exit()
+
+print(response.text)
+
 
