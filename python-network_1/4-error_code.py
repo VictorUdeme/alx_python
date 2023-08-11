7ab203d2c0e06b5938ca2472e0b9a166bf7091d0
VictorUdeme <snazzydo9@gmail.com>
2023-08-10 19:22:11 -0700

    web_scrab[3]

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -6,11 +6,17 @@ module-code_erro
 import requests
 import sys
 
-url = sys.argv[1]
+def main():
+    url = sys.argv[1]
 
-response = requests.get(url)
+    response = requests.get(url)
 
-if response.status_code >= 400:
-    print(f"Error code: {response.status_code}")
-else:
-    print(response.text)
+    if response.status_code >= 400:
+        print('Error code: {}'.format(response.status_code))
+    else:
+        print('Body of the response:')
+        print('-' * 10)
+        print(response.text)
+
+if __name__ == '__main__':
+    main()
