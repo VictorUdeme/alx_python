74fb19672e72ab63bf9a1617309236a15ccc7a84
VictorUdeme <snazzydo9@gmail.com>
2023-08-11 00:01:29 -0700

    Web_Scrap[0]

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,11 +7,8 @@ import requests
 response = requests.get('https://alu-intranet.hbtn.io/status')
 
 if response.status_code == 200:
-    for line in response.text.splitlines():
-        print(f"\t- {line}")
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
 else:
     print(f"An error occurred: {response.status_code}")
-
-
-
-  
