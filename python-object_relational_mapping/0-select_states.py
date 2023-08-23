a7a46b6ca75b353261e62ebfbf3a6fe48a0219a9
Snazzydo <snazzydo9@gmail.com>
2023-08-23 21:59:02 +0100

    Task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,61 @@
-import MySQLdb
+import mysql.connector
 
+# Function to connect to the MySQL server
+def connect_to_mysql(username, password, database):
+    try:
+        # Establish a connection to the MySQL server
+        connection = mysql.connector.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            password=password,
+            database=database
+        )
+        print("Connected to MySQL server successfully")
+        return connection
+    except mysql.connector.Error as error:
+        print("Failed to connect to MySQL server: {}".format(error))
+        return None
+
+# Function to list all states from the database
+def list_states(connection):
+    if connection is None:
+        return
+
+    try:
+        # Create a cursor object to execute SQL queries
+        cursor = connection.cursor()
+
+        # Execute the SQL query to list all states
+        query = "SELECT * FROM states ORDER BY id ASC"
+        cursor.execute(query)
+
+        # Fetch all the rows returned by the query
+        rows = cursor.fetchall()
+
+        # Display the results
+        for row in rows:
+            print(row)
+
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+    except mysql.connector.Error as error:
+        print("Failed to execute the query: {}".format(error))
+
+# Main function
 def main():
-    """Lists all states from the database hbtn_0e_0_usa."""
-
+    # Get the MySQL credentials from the user
     username = input("Enter MySQL username: ")
     password = input("Enter MySQL password: ")
-    database = input("Enter MySQL database name: ")
+    database = input("Enter database name: ")
 
-    connection = MySQLdb.connect(host="localhost", port=3306, user=username, password=password, db=database)
-    cursor = connection.cursor()
+    # Connect to the MySQL server
+    connection = connect_to_mysql(username, password, database)
 
-    query = "SELECT * FROM states ORDER BY id ASC"
-    cursor.execute(query)
+    # List all states from the database
+    list_states(connection)
 
-    for row in cursor:
-        print(row)
-
-    cursor.close()
-    connection.close()
-
+# Execute the main function
 if __name__ == "__main__":
     main()
