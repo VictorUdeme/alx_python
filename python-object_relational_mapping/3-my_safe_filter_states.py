c701a700b15726a71a1d0aa49b3b943baa337417
Snazzydo <snazzydo9@gmail.com>
2023-08-24 20:20:51 +0100

    Task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument
+Displays states from hbtn_0e_0_usa where name matches the argument.
 """
 import MySQLdb
 import sys
@@ -19,7 +18,7 @@ if __name__ == "__main__":
         host="localhost",
         port=3306
     )
-    
+
     cur = db.cursor()
     query = (
         "SELECT * FROM states "
@@ -27,7 +26,7 @@ if __name__ == "__main__":
         "ORDER BY id ASC"
     )
     cur.execute(query, (state_name,))
-    
+
     rows = cur.fetchall()
     for row in rows:
         print(row)
