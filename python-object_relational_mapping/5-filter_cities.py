725a444758de836b02ec32043b49b98ab5083bb7
Snazzydo <snazzydo9@gmail.com>
2023-08-24 20:59:16 +0100

    Task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,29 +1,22 @@
-import sys
+#!/usr/bin/python3
+"""
+List all cities from the database hbtn_0e_4_usa
+"""
+
 import MySQLdb
+import sys
 
-if __name__ == '__main__':
-    
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database_name)
-
-    # Create a cursor object to execute SQL queries
-    cursor = db.cursor()
-
-    # Execute the SQL query to retrieve all cities of the given state
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
-                    JOIN states ON cities.state_id = states.id WHERE states.name = %s \
-                    ORDER BY cities.id ASC", (state_name,))
-
-    # Fetch all the rows returned by the query
-    rows = cursor.fetchall()
-
-    # Print the results in the specified format
-    print('\n'.join([f'{row[0]}: {row[1]} ({row[2]})' for row in rows]))
-
-    # Close the cursor and database connections
-    cursor.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                JOIN states ON cities.state_id = states.id WHERE states.name=%s\
+                ORDER BY cities.id ASC", (sys.argv[4],))
+    rows = cur.fetchall()
+    cities = []
+    for row in rows:
+        cities.append(row[1])
+    print(", ".join(cities))
+    cur.close()
     db.close()
