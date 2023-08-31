6b0f12d9721b9728f15f57f1a3b143e78b970178
Snazzydo <snazzydo9@gmail.com>
2023-08-31 13:09:05 +0100

    Task 0

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -6,10 +6,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
+
