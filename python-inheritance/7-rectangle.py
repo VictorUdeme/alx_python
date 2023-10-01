bad722fa8d54d67a76c65f97e823107ec6462e24
Snazzydo <snazzydo9@gmail.com>
2023-10-01 01:33:19 +0100

    task 7

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -3,35 +3,46 @@ BaseGeometry class
 """
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry 
-
+BaseGeometry = __import__('5-base_geometry').BaseGeometry 
 class Rectangle(BaseGeometry):
+   
     """
-    This class inherits from the BaseGeometry class and
-    adds two private attributes: __width and __height
+        This class inherits from the BaseGeometry class and
+        adds two private attributes: __width and __height
     """
+    def __dir__(cls):
+        # Call the parent class's __dir__ method to get the default attributes
+       attributes = super().__dir__()
+       return[item for item in attributes if item !="__init_subclass__"]
     
     def __init__(self, width, height):
         """
-        The __init__() method of this subclass validates the 
-        width and height parameters
+            The __init__() method of this subclass validate the 
+            width and height perimeters
         """
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+        self.__width = super().integer_validator("width", width)
+        self.__height = super().integer_validator("height", height)
+
 
     def area(self):
         """
-        Calculate the area of the rectangle
-        The area of a rectangle is calculated by: 
-        multiplying the Width and Height
+            This Calculate the area of the rectangle
+            The area of a rectangle is calculated by: 
+            multiplying the Width and Height
         """
         return self.__width * self.__height
     
     def __str__(self):
         """
         The __str__() method of the Rectangle class returns a string representation of the rectangle.
-        It follows this format: [Rectangle] <width>/<height>
+        it follows this format:
+            [Rectangle] <width>/<height>
         """
         return f"[Rectangle] {self.__width}/{self.__height}"
+    def __repr__(self):
+        """
+        The __repr__() method of the Rectangle class returns a representation of the rectangle that can 
+        be used to recreate the object.
+        """
+        return f"Rectangle{self.__width}, {self.__height}"
+        
