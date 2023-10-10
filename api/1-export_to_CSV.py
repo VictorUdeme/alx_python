381c7b7481c574576c964f959d1bee6a02643baf
Snazzydo <snazzydo9@gmail.com>
2023-10-10 11:24:51 +0100

    Task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,12 +3,14 @@ import json
 import requests
 import sys
 
-if __name__ == "__main__":
+if __name__ == "main":
     employee_id = sys.argv[1]
-    api_request = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-    api_request1 = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
-    pjson = api_request.json()
-    pjson1 = api_request1.json()
+    api_request = requests.get("https://jsonplaceholder.typicode.com/users/%7B%7D".format(employee_id))
+    api_request1 = requests.get("https://jsonplaceholder.typicode.com/users/%7B%7D/todos".format(employee_id))
+    data = api_request.text
+    pjson = json.loads(data)
+    data1 = api_request1.text
+    pjson1 = json.loads(data1)
 
     #export data to csv data
     filename = "{}.csv".format(employee_id)
