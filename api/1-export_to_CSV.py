3e699a74d14fd28a205c849cae9032303c6efc81
Snazzydo <snazzydo9@gmail.com>
2023-10-10 11:23:32 +0100

    Task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,47 +1,22 @@
 import csv
-import os
+import json
 import requests
 import sys
 
-def user_info(employee_id):
-    # Make API requests to retrieve user and tasks data
-    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-    tasks_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
-
-    if user_response.status_code != 200 or tasks_response.status_code != 200:
-        print("Error: Failed to retrieve data from the API.")
-        sys.exit(1)
-
-    user_data = user_response.json()
-    tasks_data = tasks_response.json()
-
-    # Create a CSV file and write the data
-    filename = f"{employee_id}.csv"
-    with open(filename, 'w', newline='') as csvfile:
-        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-
-        for task in tasks_data:
-            user_id = employee_id
-            username = user_data['username']
-            task_completed_status = task['completed']
-            task_title = task['title']
-            writer.writerow([user_id, username, task_completed_status, task_title])
-
-    print(f"CSV file '{filename}' has been created.")
-
-    # Check if the CSV file exists and read/process it here if needed
-    if os.path.exists(filename):
-        # Read and process the CSV file here if needed
-        pass
-    else:
-        print(f"CSV file '{filename}' does not exist.")
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
     employee_id = sys.argv[1]
-    user_info(employee_id)
+    api_request = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    api_request1 = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
+    pjson = api_request.json()
+    pjson1 = api_request1.json()
+
+    #export data to csv data
+    filename = "{}.csv".format(employee_id)
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting = csv.QUOTE_ALL)
+        for item in pjson1:
+            user_id = employee_id
+            username = pjson['username']
+            task_completed_status = item['completed']
+            task_title = item['title']
+            writer.writerow([user_id, username, task_completed_status, task_title])
